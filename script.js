bef37c95ba11dfe8a92e106402b0e3bf90cfbe10
kaiserleonardo <kaiser.leonardo@escola.pr.gov.br>
2026-05-20 14:09:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // 1. Configuração (Substitua pela URL e Key da nova API escolhida)
-const API_URL = "https://gen.pollinations.ai/image/a%20cat%20in%20space?model=flux"; 
-const API_KEY = "sk_4soEPmFACFCtPVWdDoE7wJkZcoAJfH64";
+const API_URL = https://gen.pollinations.ai/image/a%20cat%20in%20space?model=flux ; 
+const API_KEY = sk_4soEPmFACFCtPVWdDoE7wJkZcoAJfH64;
 
 // 2. Função Principal
 document.getElementById('btnVivificar').addEventListener('click', async () => {
