37b4744963571d28113e7db0664bd31a931eba1d
kaiserleonardo <kaiser.leonardo@escola.pr.gov.br>
2026-05-20 14:27:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // 1. Configuração (Substitua pela URL e Key da nova API escolhida)
 const API_URL = https://gen.pollinations.ai; 
-const API_KEY = sk_4soEPmFACFCtPVWdDoE7wJkZcoAJfH64;
+const API_KEY = sk_6Rf53sRduQFXpJYLWDgGhbNFXkHp9XnF;
 
 // 2. Função Principal
 document.getElementById('btnVivificar').addEventListener('click', async () => {
